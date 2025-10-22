c4c0e7d6d90c79172e215d4d474916739c36bff1
mutiu4luv <chidiemmamadu@gmail.com>
2025-10-22 23:22:54 +0100

    first commit

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -6,9 +6,31 @@ import {
   Box,
   Button,
   Container,
+  IconButton,
+  Menu,
+  MenuItem,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const menuItems = [
+    { label: "Home", href: "#home" },
+    { label: "Offers", href: "#offers" },
+    { label: "Special Offer", href: "#special" },
+    { label: "Awards", href: "#awards" },
+    { label: "Contact", href: "#contact" },
+  ];
+
   return (
     <AppBar
       position="static"
@@ -21,6 +43,7 @@ const Navbar = () => {
           disableGutters
           sx={{ display: "flex", justifyContent: "space-between" }}
         >
+          {/* Logo Section */}
           <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
             <Box
               sx={{
@@ -42,13 +65,61 @@ const Navbar = () => {
             </Typography>
           </Box>
 
+          {/* Desktop Menu */}
           <Box sx={{ display: { xs: "none", md: "flex" }, gap: 2 }}>
-            <Button href="#courses">Home</Button>
-            <Button href="#offers">Offers</Button>
-            <Button href="#founder">Special Offer</Button>
-            <Button href="#founder">Awards</Button>
-            <Button href="#founder">Contact</Button>
+            {menuItems.map((item) => (
+              <Button
+                key={item.label}
+                href={item.href}
+                sx={{
+                  color: "#065f46",
+                  textTransform: "none",
+                  fontWeight: 600,
+                }}
+              >
+                {item.label}
+              </Button>
+            ))}
           </Box>
+
+          {/* Mobile Menu Icon */}
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              edge="end"
+              color="inherit"
+              onClick={handleOpenMenu}
+            >
+              <MenuIcon sx={{ color: "#065f46" }} />
+            </IconButton>
+          </Box>
+
+          {/* Mobile Dropdown Menu */}
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleCloseMenu}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            {menuItems.map((item) => (
+              <MenuItem
+                key={item.label}
+                onClick={handleCloseMenu}
+                component="a"
+                href={item.href}
+                sx={{ color: "#065f46", fontWeight: 500 }}
+              >
+                {item.label}
+              </MenuItem>
+            ))}
+          </Menu>
         </Toolbar>
       </Container>
     </AppBar>
